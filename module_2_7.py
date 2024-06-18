87ea2073c722ec2a4da43e76908a9289082cf799
Grivmoon <vadikaisman@yandex.ru>
2024-06-18 18:23:11 +0700

    домашнее задание

--- module_2_7.py
+++ module_2_7.py
@@ -1,18 +1,7 @@
-def print_params(a=1, b="строка", c=True):
-    print(a, b, c)
-
-print_params()
-print_params(2, 2, 2)
-print_params(b=25)
-print_params(c=[1, 2, 3])
+def get_multiplied_digits(number=1):
+    if len(str(number)) == 1:
+        return number
+    return int(str(number)[0]) * get_multiplied_digits(int(str(number)[1:]))
 
 
-values_list = [5, 'список', True]
-values_dict = {'a': 8, 'b': 'перечень', 'c': True}
-
-print_params(*values_list)
-print_params(**values_dict)
-
-
-values_list_2 = [54.32, 'Строка']
-print_params(*values_list_2, 42)
+print(get_multiplied_digits(145745745845))

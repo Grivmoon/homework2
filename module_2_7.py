f7272205982cbe600bfb13c39e022bedc8669ec8
Grivmoon <vadikaisman@yandex.ru>
2024-06-21 20:38:31 +0700

    домашнее задание

--- module_2_7.py
+++
@@ -1,7 +0,0 @@
-def get_multiplied_digits(number=1):
-    if len(str(number)) == 1:
-        return number
-    return int(str(number)[0]) * get_multiplied_digits(int(str(number)[1:]))
-
-
-print(get_multiplied_digits(145745745845))

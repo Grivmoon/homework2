1bc24459ad15db980455f562f0e44f9d434720d5
Grivmoon <vadikaisman@yandex.ru>
2024-06-23 20:00:51 +0700

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-name = input("Введите ваше имя: ")
-age = int(input("Введите ваш возраст: "))
-favorite_color = input("Введите ваш любимый цвет: ")
-
-future_age = age + 5
-
-print("\nПривет, " + name + "!")
-print(f"Тебе {age} лет, через 5 лет тебе будет {future_age}.")
-print("Твой любимый цвет:", favorite_color)
